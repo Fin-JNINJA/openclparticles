5d3c39c1626d7aa22a942f2ce49af7d7e0cfbb12
Fin-JNINJA <finlay.jolley@gmail.com>
2026-05-23 08:44:04 +0100

    Add project files.

---
+++ openclparticles.cpp
@@ -0,0 +1,12 @@
+﻿// openclparticles.cpp : Defines the entry point for the application.
+//
+
+#include "openclparticles.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
