19b3a09f5799d315ffcb9004899605ecf80331e1
Fin-JNINJA <finlay.jolley@gmail.com>
2026-06-12 00:13:01 +0100

    refactor and timer

--- src/openclparticles.cpp
+++ src/openclparticles.cpp
@@ -1,7 +1,6 @@
 ﻿// openclparticles.cpp : Defines the entry point for the application.
 //
 
-
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN 0
 #endif
@@ -9,12 +8,17 @@
 #include <wx/wxprec.h>
 
 #ifndef WX_PRECOMP
+#include <wx/sizer.h>
 #include <wx/wx.h>
+#include <wx/timer.h>
 #endif
 
 #include <CL/cl.hpp>
 
-//#include "../static/teapot.xpm"
+enum
+{
+    ID_Hello = 1
+};
 
 using namespace std;
 
@@ -44,23 +48,6 @@ public:
     DECLARE_EVENT_TABLE()
 };
 
-BEGIN_EVENT_TABLE(wxImagePanel, wxPanel)
-// some useful events
-/*
-EVT_MOTION(wxImagePanel::mouseMoved)
-EVT_LEFT_DOWN(wxImagePanel::mouseDown)
-EVT_LEFT_UP(wxImagePanel::mouseReleased)
-EVT_RIGHT_DOWN(wxImagePanel::rightClick)
-EVT_LEAVE_WINDOW(wxImagePanel::mouseLeftWindow)
-EVT_KEY_DOWN(wxImagePanel::keyPressed)
-EVT_KEY_UP(wxImagePanel::keyReleased)
-EVT_MOUSEWHEEL(wxImagePanel::mouseWheelMoved)
-*/
-
-// catch paint events
-EVT_PAINT(wxImagePanel::paintEvent)
-
-END_EVENT_TABLE()
 
 wxImagePanel::wxImagePanel(wxFrame* parent, wxString file, wxBitmapType format) :
     wxPanel(parent)
@@ -68,12 +55,6 @@ wxImagePanel::wxImagePanel(wxFrame* parent, wxString file, wxBitmapType format) 
     // load the file... ideally add a check to see if loading was successful
     image.LoadFile(file, format);
 }
-
-/*
- * Called by the system of by wxWidgets when the panel needs
- * to be redrawn. You can also trigger this call by
- * calling Refresh()/Update().
- */
 
 void wxImagePanel::paintEvent(wxPaintEvent& evt)
 {
@@ -95,89 +76,77 @@ void wxImagePanel::render(wxDC& dc)
     dc.DrawBitmap(image, 0, 0, false);
 }
 
-class MyApp : public wxApp
+
+BEGIN_EVENT_TABLE(wxImagePanel, wxPanel)
+
+EVT_PAINT(wxImagePanel::paintEvent)
+
+END_EVENT_TABLE()
+
+class RenderTimer : public wxTimer
 {
+    wxImagePanel* pane;
 public:
-    virtual bool OnInit();
+    RenderTimer(wxImagePanel* pane);
+    void Notify();
+    void start();
 };
+
+RenderTimer::RenderTimer(wxImagePanel* pane) : wxTimer()
+{
+    RenderTimer::pane = pane;
+}
+
+void RenderTimer::Notify()
+{
+    pane->Refresh();
+}
+
+void RenderTimer::start()
+{
+    wxTimer::Start(16);
+}
 
 class MyFrame : public wxFrame
 {
+    RenderTimer* timer;
+    wxImagePanel* drawPane;
+
 public:
     MyFrame();
-
+    ~MyFrame()
+    {
+        delete timer;
+    }
+    void onClose(wxCloseEvent& evt)
+    {
+        timer->Stop();
+        evt.Skip();
+    }
+    DECLARE_EVENT_TABLE()
 private:
     void OnHello(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
-	void OnPaint(wxPaintEvent& event);
+    void OnPaint(wxPaintEvent& event);
 };
 
-enum
+BEGIN_EVENT_TABLE(MyFrame, wxFrame)
+EVT_CLOSE(MyFrame::onClose)
+END_EVENT_TABLE()
+
+MyFrame::MyFrame() : wxFrame((wxFrame*)NULL, -1, wxT("Hello wxDC"), wxPoint(50, 50), wxSize(1920, 1080))
 {
-    ID_Hello = 1
-};
-
-wxIMPLEMENT_APP(MyApp);
-
-bool MyApp::OnInit()
-{
-    MyFrame* frame = new MyFrame();
-    wxImagePanel* drawPane;
-
-    wxInitAllImageHandlers();
-
     wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
-    //frame = new wxFrame(NULL, wxID_ANY, wxT("Hello wxDC"), wxPoint(50, 50), wxSize(800, 600));
-
-    // then simply create like this
-    drawPane = new wxImagePanel(frame, wxT("C:\\Users\\Fin\\Documents\\place to clone repositories\\openclparticles\\static\\teapot.xpm"), wxBITMAP_TYPE_XPM);
+    drawPane = new wxImagePanel(this, wxT("C:\\Users\\finla\\Documents\\CodeProjects\\openclparticles\\static\\teapot.xpm"), wxBITMAP_TYPE_XPM);
     sizer->Add(drawPane, 1, wxEXPAND);
+    SetSizer(sizer);
 
-    frame->SetSizer(sizer);
+    timer = new RenderTimer(drawPane);
+    Show();
+    timer->start();
 
-    frame->Show(true);
 
-    //OPENCL SETUP 
-
-    cl_uint numPlatforms;
-    clGetPlatformIDs(0, nullptr, &numPlatforms);
-
-    // Step 2: Get the first available platform
-    cl_platform_id platform;
-    clGetPlatformIDs(1, &platform, nullptr);
-
-    // Step 3: Get number of GPU devices available on the platform
-    cl_uint numDevices;
-    clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 0, nullptr, &numDevices);
-
-    // Step 4: Get the first GPU device ID
-    cl_device_id device;
-    clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, nullptr);
-
-    // Step 5: Define buffers to store device info
-    char name[128], vendor[128];
-    cl_ulong mem;
-    cl_uint units;
-
-    // Step 6: Retrieve device name, vendor, memory size, and compute units
-    clGetDeviceInfo(device, CL_DEVICE_NAME, sizeof(name), name, nullptr);
-    clGetDeviceInfo(device, CL_DEVICE_VENDOR, sizeof(vendor), vendor, nullptr);
-    clGetDeviceInfo(device, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(mem), &mem, nullptr);
-    clGetDeviceInfo(device, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(units), &units, nullptr);
-
-    // Step 7: Print the collected device information
-    std::cout << "Device Name: " << name << "\n";
-    std::cout << "Vendor: " << vendor << "\n";
-    std::cout << "Memory: " << mem / (1024 * 1024) << " MB\n";
-    std::cout << "Compute Units: " << units << "\n";
-
-    return true;
-}
-
-MyFrame::MyFrame()
-    : wxFrame(NULL, wxID_ANY, "Hello World")
-{
     wxMenu* menuFile = new wxMenu;
     menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
         "Help string shown in status bar for this menu item");
@@ -217,4 +186,32 @@ void MyFrame::OnHello(wxCommandEvent& event)
 {
     wxLogMessage("Hello world from wxWidgets!");
 }
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+bool MyApp::OnInit()
+{
+    wxInitAllImageHandlers();
+    MyFrame* frame = new MyFrame();
+    //pass wxImagePanel* drawPane;
+
+    wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+    //frame = new wxFrame(NULL, wxID_ANY, wxT("Hello wxDC"), wxPoint(50, 50), wxSize(800, 600));
+
+    //// then simply create like this
+    //drawPane = new wxImagePanel(frame, wxT("C:\\Users\\finla\\Documents\\CodeProjects\\openclparticles\\static\\teapot.xpm"), wxBITMAP_TYPE_XPM);
+    //sizer->Add(drawPane, 1, wxEXPAND);
+
+    frame->SetSizer(sizer);
+
+    frame->Show(true);
+
+    return true;
+}
+
+wxIMPLEMENT_APP(MyApp);
 
